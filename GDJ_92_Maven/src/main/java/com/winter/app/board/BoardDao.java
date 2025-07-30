734154be335b13e24a2df1a0f7882e8baf0240c9
huni99 <wjdrlfgns1@naver.com>
2025-07-30 17:28:44 +0900

    정길훈
    mybatis 연결

--- GDJ_92_Maven/src/main/java/com/winter/app/board/BoardDao.java
+++ GDJ_92_Maven/src/main/java/com/winter/app/board/BoardDao.java
@@ -4,4 +4,8 @@ public interface BoardDao {
 	
 	//insert
 	public int insert(BoardVO vo)throws Exception;
+	//update
+	public int update(BoardVO vo)throws Exception;
+	//delete
+	public int delete(int boardNum)throws Exception;
 }
---
+++ GDJ_92_Maven/src/test/java/com/winter/app/board/notice/NoticeDAOTest.java
@@ -0,0 +1,47 @@
+package com.winter.app.board.notice;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeDAOTest {
+	
+	@Autowired
+	private NoticeDAO noticeDao;
+	
+	
+//	@Test
+//	void insertTest() throws Exception{
+//		NoticeVO noticeVO =new NoticeVO();
+//		noticeVO.setBoardContents("content3");
+//		noticeVO.setBoardTitle("title3");
+//		noticeVO.setBoardWriter("writer3");
+//		int result = noticeDao.insert(noticeVO);
+//		
+//		
+//		//단정문 
+//		assertEquals(1, result);
+//	}
+//	@Test
+//	void updateTest() throws Exception{
+//		NoticeVO noticeVO =new NoticeVO();
+//		noticeVO.setBoardContents("content4");
+//		noticeVO.setBoardTitle("title4");
+//		noticeVO.setBoardWriter("writer4");
+//		int result = noticeDao.update(noticeVO);
+//		
+//		assertEquals(1, result);
+//	
+//	}
+	@Test
+	void deleteTest() throws Exception{
+		int boardNum=5;
+		int result = noticeDao.delete(boardNum);
+	
+		assertEquals(1, result);
+	
+	}
+}
